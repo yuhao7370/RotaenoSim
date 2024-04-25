3b1ea0559096fbf5551403f7c74079ca9c1cf810
yuhao7370 <53969724+yuhao7370@users.noreply.github.com>
2024-04-25 19:54:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -115,7 +115,7 @@ startButton.addEventListener('click', function() {
             
             // var flag = false;
             if(beta < 0) {
-            gameangledeg = 360 - gameangledeg;
+            gameangledeg = gameangledeg - 180;
                 // flag = true;
             }
             vectorElement.textContent = 'Vector: [' + vector[0].toFixed(2) + ', ' + vector[1].toFixed(2) + ', ' + vector[2].toFixed(2) + ']';
