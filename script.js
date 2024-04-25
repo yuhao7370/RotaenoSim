6ae4894490cd7ee6991b4ff0e6818e801eac0ecd
yuhao7370 <53969724+yuhao7370@users.noreply.github.com>
2024-04-25 19:51:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -114,10 +114,10 @@ startButton.addEventListener('click', function() {
             var gameangledeg = gameangle * 180 / Math.PI; // rad to degree
             
             // var flag = false;
-            // if(beta > 0) {
+            if(beta < 0) {
             gameangledeg = 360 - gameangledeg;
-            //     flag = true;
-            // }
+                flag = true;
+            }
             vectorElement.textContent = 'Vector: [' + vector[0].toFixed(2) + ', ' + vector[1].toFixed(2) + ', ' + vector[2].toFixed(2) + ']';
             angleElement.textContent = 'Angle: ' + gameangledeg.toFixed(2) + '°';
 
