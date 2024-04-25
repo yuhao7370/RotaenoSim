6c982b3c30585d9bfdeb00d92b6fc628c11e99c5
yuhao7370 <53969724+yuhao7370@users.noreply.github.com>
2024-04-25 19:32:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -103,7 +103,7 @@ startButton.addEventListener('click', function() {
             z = [0, 0, 1]
             var anglez = calc_vector_angle(vector, z); // 计算手机屏幕平面与z轴的夹角 rad
             var anglezdeg = anglez * 180 / Math.PI; // rad to degree
-            var zvector = [0, 0, 1 / cos(anglez)]; // z轴在手机屏幕平面中的投影
+            var zvector = [0, 0, 1 / Math.cos(anglez)]; // z轴在手机屏幕平面中的投影
             var basevactor = [zvector[0] - vector[0], zvector[1] - vector[1], zvector[2] - vector[2]]; // 游戏基准向量
             var splitvector = calculateScreenSplitLineVector(alpha, beta, gamma); // 分割线的向量
             var gameangle = calc_vector_angle(basevactor, splitvector); // 游戏基准向量与分割线的夹角
